20b17fba4f781db8c77a529b6a2d01111a1881a6
Steve Arnold <nerdboy@gentoo.org>
2019-04-07 17:15:45 -0700

    still updating build scripts and configs for proper versioning
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ ARCH=$(uname -m)
 
 config="defconfig"
 
-build_prefix="-aarch64-r"
+build_prefix="-aarch64-x"
 branch_prefix="v"
 branch_postfix=".x"
 
@@ -13,19 +13,28 @@ branch_postfix=".x"
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_6"
+#toolchain="gcc_linaro_eabi_7"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
+#toolchain="gcc_linaro_gnueabihf_6"
+#toolchain="gcc_linaro_gnueabihf_7"
+toolchain="gcc_arm_gnueabihf_8"
+
 #arm64
 KERNEL_ARCH=arm64
-toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_6"
+#toolchain="gcc_linaro_aarch64_gnu_7"
+toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel/Build
 KERNEL_REL=5.0
 KERNEL_TAG=${KERNEL_REL}.7
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
@@ -35,6 +44,6 @@ kernel_rt=".X-rtY"
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
-DISTRO=cross
+DISTRO=xross
 DEBARCH=arm64
 #
