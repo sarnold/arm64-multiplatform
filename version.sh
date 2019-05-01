1c746da160d98125f1516485ecd5a7beafdf5ea9
Steve Arnold <nerdboy@gentoo.org>
2019-04-30 21:44:14 -0700

    update to 5.0.10-aarch64-x0
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -33,8 +33,8 @@ toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel/Build
 KERNEL_REL=5.0
-KERNEL_TAG=${KERNEL_REL}.9
-BUILD=${build_prefix}4
+KERNEL_TAG=${KERNEL_REL}.10
+BUILD=${build_prefix}0
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
