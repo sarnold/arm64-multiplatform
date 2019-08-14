6ab26b7aa207a3b69a97d8bddcfc91289e1d8a8d
Steve Arnold <nerdboy@gentoo.org>
2019-08-13 17:43:53 -0700

    bump tag to 5.2.7 and reset patch dir (keep usb phy and pine64)
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- patch.sh
+++ patch.sh
@@ -103,71 +103,22 @@ local_patch () {
 	${git} "${DIR}/patches/dir/0001-patch.patch"
 }
 
-meson64 () {
-	echo "dir: meson64"
-	${git} "${DIR}/patches/meson64/0001-ARM64-defconfig-enable-CEC-support.patch"
-	${git} "${DIR}/patches/meson64/0002-ASoC-meson-add-meson-audio-core-driver.patch"
-	${git} "${DIR}/patches/meson64/0003-ASoC-meson-add-register-definitions.patch"
-	${git} "${DIR}/patches/meson64/0004-ASoC-meson-add-aiu-i2s-dma-support.patch"
-	${git} "${DIR}/patches/meson64/0005-ASoC-meson-add-initial-i2s-dai-support.patch"
-	${git} "${DIR}/patches/meson64/0006-ASoC-meson-add-aiu-spdif-dma-support.patch"
-	${git} "${DIR}/patches/meson64/0007-ASoC-meson-add-initial-spdif-dai-support.patch"
-	${git} "${DIR}/patches/meson64/0008-ARM64-defconfig-enable-audio-support-for-meson-SoCs-.patch"
-	${git} "${DIR}/patches/meson64/0009-ARM64-dts-meson-gx-add-audio-controller-nodes.patch"
-	${git} "${DIR}/patches/meson64/0010-snd-meson-activate-HDMI-audio-path.patch"
-	${git} "${DIR}/patches/meson64/0011-drm-meson-select-dw-hdmi-i2s-audio-for-meson-hdmi.patch"
-	${git} "${DIR}/patches/meson64/0012-ARM64-dts-meson-gx-add-sound-dai-cells-to-HDMI-node.patch"
-	${git} "${DIR}/patches/meson64/0013-ARM64-dts-meson-activate-hdmi-audio-HDMI-enabled-boa.patch"
-	${git} "${DIR}/patches/meson64/0014-drm-bridge-dw-hdmi-Use-AUTO-CTS-setup-mode-when-non-.patch"
-	${git} "${DIR}/patches/meson64/0017-soc-amlogic-add-meson-canvas-driver.patch"
-	${git} "${DIR}/patches/meson64/0018-ARM64-dts-meson-gx-add-dmcbus-and-canvas-nodes.patch"
-	${git} "${DIR}/patches/meson64/0020-drm-meson-Use-optional-canvas-provider.patch"
-	${git} "${DIR}/patches/meson64/0021-arm64-dts-meson-gx-Add-canvas-provider-node-to-the-v.patch"
-	${git} "${DIR}/patches/meson64/0022-drm-meson-Support-Overlay-plane-for-video-rendering.patch"
-	${git} "${DIR}/patches/meson64/0023-drm-meson-move-OSD-scaler-management-into-plane-atom.patch"
-	${git} "${DIR}/patches/meson64/0024-drm-meson-Add-primary-plane-scaling.patch"
-	${git} "${DIR}/patches/meson64/0026-pinctrl-meson-gxl-remove-invalid-GPIOX-tsin_a-pins.patch"
-	${git} "${DIR}/patches/meson64/0027-arm64-dts-meson-gx-Add-hdmi_5v-regulator-as-hdmi-tx-.patch"
-	${git} "${DIR}/patches/meson64/0028-arm64-dts-meson-gxl-libretech-cc-fix-GPIO-lines-name.patch"
-	${git} "${DIR}/patches/meson64/0029-arm64-dts-meson-gxbb-nanopi-k2-fix-GPIO-lines-names.patch"
-	${git} "${DIR}/patches/meson64/0030-arm64-dts-meson-gxbb-odroidc2-fix-GPIO-lines-names.patch"
-	${git} "${DIR}/patches/meson64/0031-arm64-dts-meson-gxl-khadas-vim-fix-GPIO-lines-names.patch"
-	${git} "${DIR}/patches/meson64/0032-drm-meson-Add-support-for-VIC-alternate-timings.patch"
-	${git} "${DIR}/patches/meson64/0033-media-meson-add-v4l2-m2m-video-decoder-driver.patch"
-	${git} "${DIR}/patches/meson64/0034-MAINTAINERS-Add-meson-video-decoder.patch"
-	${git} "${DIR}/patches/meson64/0035-arm64-dts-meson-gx-add-vdec-entry.patch"
-	${git} "${DIR}/patches/meson64/0036-arm64-dts-meson-add-vdec-entries.patch"
-	${git} "${DIR}/patches/meson64/0037-meson-vdec-introduce-controls-and-V4L2_CID_MIN_BUFFE.patch"
-	${git} "${DIR}/patches/meson64/0038-media-videodev2-add-V4L2_FMT_FLAG_NO_SOURCE_CHANGE.patch"
-	${git} "${DIR}/patches/meson64/0039-meson-vdec-allow-subscribing-to-V4L2_EVENT_SOURCE_CH.patch"
-	${git} "${DIR}/patches/meson64/0040-media-meson-vdec-add-H.264-decoding-support.patch"
-	${git} "${DIR}/patches/meson64/0041-media-meson-vdec-add-MPEG4-decoding-support.patch"
-	${git} "${DIR}/patches/meson64/0042-media-meson-vdec-add-MJPEG-decoding-support.patch"
-	${git} "${DIR}/patches/meson64/0043-clk-meson-gxbb-set-fclk_div3-as-CLK_IS_CRITICAL.patch"
-	${git} "${DIR}/patches/meson64/0008-drm-meson-Add-HDMI-1.4-4k-modes.patch"
-	${git} "${DIR}/patches/meson64/0009-drm-meson-Use-drm_fbdev_generic_setup.patch"
-	${git} "${DIR}/patches/meson64/0010-fixup-drm-meson-Use-optional-canvas-provider.patch"
-	${git} "${DIR}/patches/meson64/0014-drm-bridge-dw-hdmi-Add-SCDC-and-TMDS-Scrambling-supp.patch"
-	${git} "${DIR}/patches/meson64/0015-drm-meson-add-HDMI-div40-TMDS-mode.patch"
-	${git} "${DIR}/patches/meson64/0016-drm-meson-add-support-for-HDMI2.0-2160p-modes.patch"
-	${git} "${DIR}/patches/meson64/0017-drm-bridge-dw-hdmi-add-support-for-YUV420-output.patch"
-	${git} "${DIR}/patches/meson64/0018-drm-bridge-dw-hdmi-support-dynamically-get-input-out.patch"
-	${git} "${DIR}/patches/meson64/0019-drm-bridge-dw-hdmi-allow-ycbcr420-modes-for-0x200a.patch"
-	${git} "${DIR}/patches/meson64/0020-drm-meson-Add-YUV420-output-support.patch"
-	${git} "${DIR}/patches/meson64/0021-drm-meson-Output-in-YUV444-if-sink-supports-it.patch"
-	${git} "${DIR}/patches/meson64/0023-drm-meson-Fix-an-Alpha-Primary-Plane-bug-on-Meson-GX.patch"
-}
-
 usb_phy () {
 	echo "dir: usb_phy"
 	${git} "${DIR}/patches/meson64/0001-ARM64-dts-meson-gxbb-odroidc2-Fix-usb-phy-reset-warning.patch"
 	${git} "${DIR}/patches/meson64/0002-ARM64-dts-meson-gxbb-odroidc2-Fix-usb-phy-regulator-power-failed-warning.patch"
 }
 
+pine64 () {
+	echo "dir: pine64"
+	${git} "${DIR}/patches/pine64/0001-ARCH-arm64-dts-sun50i-a64-enable-pinebook-backlight.patch"
+#	${git} "${DIR}/patches/pine64/0003-net-stmmac-dwmac-sun8i-support-RGMII-modes-with-PHY-.patch"
+#	${git} "${DIR}/patches/pine64/0004-arm64-allwinner-a64-disable-the-RTL8211E-internal-RX.patch"
+}
+
 #external_git
 #rt
 #local_patch
-#meson64
 usb_phy
 
 packaging () {
--- version.sh
+++ version.sh
@@ -32,9 +32,9 @@ KERNEL_ARCH=arm64
 toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel/Build
-KERNEL_REL=5.0
-KERNEL_TAG=${KERNEL_REL}.7
-BUILD=${build_prefix}4
+KERNEL_REL=5.2
+KERNEL_TAG=${KERNEL_REL}.8
+BUILD=${build_prefix}0
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
