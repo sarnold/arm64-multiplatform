3c72d8f19cda6cf0c7881785c79c3f5ee0111ef9
Steve Arnold <nerdboy@gentoo.org>
2019-09-27 12:57:43 -0700

    Update version.sh, reset on branch v5.3.x (patches still TBD)
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -32,9 +32,9 @@ KERNEL_ARCH=arm64
 toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel/Build
-KERNEL_REL=5.2
-KERNEL_TAG=${KERNEL_REL}.9
-BUILD=${build_prefix}3
+KERNEL_REL=5.3
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=${build_prefix}0
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
